87e945f7bbfa93bb84e15d99c238f307afeb5fd0
Kyle <kyle.allbright@gmail.com>
2019-12-05 18:55:11 +0000

    Add project files.

---
+++ MarkType.cs
@@ -0,0 +1,21 @@
+﻿namespace TicTacToe
+{
+    /// <summary>
+    /// This type of value a cel in the game is currently at
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// The cell has not been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The cell is O
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// The cell is an X
+        /// </summary>
+        Cross
+    }
+}
